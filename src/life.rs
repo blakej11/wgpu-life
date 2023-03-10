3c10e8f0bcbfe870a3474bf37836982ea9198658
Darkclainer <Darkclainer@gmail.com>
2023-03-10 16:00:57 +0400

    Update wgpu and winit to fix wayland startup issues

--- src/life.rs
+++ src/life.rs
@@ -1,6 +1,6 @@
 // Conway's Game of Life
 
-use std::{borrow::Cow, mem};
+use std::mem;
 use wgpu::util::DeviceExt;
 
 use crate::{
@@ -36,24 +36,25 @@ impl Life {
         texture: &Texture,
     ) -> Self {
         // Load and compile the compute shader.
-        let compute_shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+        let compute_shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: None,
-            source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("life.wgsl"))),
+            source: wgpu::ShaderSource::Wgsl(include_str!("life.wgsl").into()),
         });
 
         // Allocate a pair of equal-sized GPU buffers to hold cell data.
         // COPY_SRC is used so they can be read from for debugging.
         let cell_bufsize = dimensions.area() * mem::size_of::<f32>();
-        let cell_buffers = RenderSources::new(|dir|
+        let cell_buffers = RenderSources::new(|dir| {
             device.create_buffer(&wgpu::BufferDescriptor {
                 label: Some(&format!("Source for {:?}", dir)),
                 usage: wgpu::BufferUsages::VERTEX
-                     | wgpu::BufferUsages::STORAGE
-                     | wgpu::BufferUsages::COPY_SRC
-                     | wgpu::BufferUsages::COPY_DST,
+                    | wgpu::BufferUsages::STORAGE
+                    | wgpu::BufferUsages::COPY_SRC
+                    | wgpu::BufferUsages::COPY_DST,
                 size: cell_bufsize as _,
                 mapped_at_creation: false,
-            }));
+            })
+        });
 
         // Create the bind group layout and compute pipeline for the life algorithm.
         let compute_bind_group_layout =
@@ -66,7 +67,6 @@ impl Life {
                         ty: params.binding_type(),
                         count: None,
                     },
-
                     // Binding for the global variable "cellSrc".
                     wgpu::BindGroupLayoutEntry {
                         binding: 1,
@@ -74,12 +74,10 @@ impl Life {
                         ty: wgpu::BindingType::Buffer {
                             ty: wgpu::BufferBindingType::Storage { read_only: true },
                             has_dynamic_offset: false,
-                            min_binding_size:
-                                wgpu::BufferSize::new(cell_bufsize as _),
+                            min_binding_size: wgpu::BufferSize::new(cell_bufsize as _),
                         },
                         count: None,
                     },
-
                     // Binding for the global variable "cellDst".
                     wgpu::BindGroupLayoutEntry {
                         binding: 2,
@@ -87,12 +85,10 @@ impl Life {
                         ty: wgpu::BindingType::Buffer {
                             ty: wgpu::BufferBindingType::Storage { read_only: false },
                             has_dynamic_offset: false,
-                            min_binding_size:
-                                wgpu::BufferSize::new(cell_bufsize as _),
+                            min_binding_size: wgpu::BufferSize::new(cell_bufsize as _),
                         },
                         count: None,
                     },
-
                     // Binding for the global variable "texture".
                     wgpu::BindGroupLayoutEntry {
                         binding: 3,
@@ -117,7 +113,7 @@ impl Life {
         });
 
         // Create a RenderMotion of bind groups to map the RenderSources of cell_buffers.
-        let bind_groups = RenderMotion::new(|dir|
+        let bind_groups = RenderMotion::new(|dir| {
             device.create_bind_group(&wgpu::BindGroupDescriptor {
                 layout: &compute_bind_group_layout,
                 entries: &[
@@ -140,7 +136,7 @@ impl Life {
                 ],
                 label: None,
             })
-        );
+        });
 
         Life {
             compute_pipeline,
@@ -152,17 +148,14 @@ impl Life {
     }
 
     /// update is called for any WindowEvent not handled by the framework
-    fn _update(
-        &mut self,
-         _event: winit::event::WindowEvent,
-    ) {
+    fn _update(&mut self, _event: winit::event::WindowEvent) {
         // empty
     }
 
     /// resize is called on WindowEvent::Resized events
     fn _resize(
         &mut self,
-        _sc_desc: &wgpu::SwapChainDescriptor,
+        _sc_desc: &wgpu::SurfaceConfiguration,
         _device: &wgpu::Device,
         _queue: &wgpu::Queue,
     ) {
@@ -170,45 +163,38 @@ impl Life {
     }
 
     // Import some data into the Life grid.
-    pub fn import(
-        &self,
-        device: &wgpu::Device,
-        queue: &wgpu::Queue,
-        cells: Vec<f32>,
-    ) {
+    pub fn import(&self, device: &wgpu::Device, queue: &wgpu::Queue, cells: Vec<f32>) {
         assert_eq!(cells.len(), self.dimensions.area());
 
         let import_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("cell data import buffer"),
             usage: wgpu::BufferUsages::VERTEX
-                 | wgpu::BufferUsages::STORAGE
-                 | wgpu::BufferUsages::COPY_SRC,
+                | wgpu::BufferUsages::STORAGE
+                | wgpu::BufferUsages::COPY_SRC,
             contents: bytemuck::cast_slice(&cells),
-            });
+        });
 
-        let mut command_encoder =
-            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("filling Life grid")
-            });
+        let mut command_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("filling Life grid"),
+        });
 
         let next_src = &self.cell_buffers.src(RenderDir::dir(self.frame_num));
 
         command_encoder.copy_buffer_to_buffer(
-            &import_buf, 0, next_src, 0,
-            bytemuck::cast_slice::<_, u8>(&cells).len() as u64
+            &import_buf,
+            0,
+            next_src,
+            0,
+            bytemuck::cast_slice::<_, u8>(&cells).len() as u64,
         );
 
         queue.submit(Some(command_encoder.finish()));
     }
 
-    pub fn step(
-        &mut self,
-        command_encoder: &mut wgpu::CommandEncoder,
-    ) {
-        let mut cpass =
-            command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                label: Some("Life grid step")
-            });
+    pub fn step(&mut self, command_encoder: &mut wgpu::CommandEncoder) {
+        let mut cpass = command_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+            label: Some("Life grid step"),
+        });
 
         let xdim = self.dimensions.width() + WORKGROUP_SIZE.0 - 1;
         let xgroups = xdim / WORKGROUP_SIZE.0;
@@ -217,8 +203,9 @@ impl Life {
         let dir = RenderDir::dir(self.frame_num);
 
         cpass.set_pipeline(&self.compute_pipeline);
-        cpass.set_bind_group(0, &self.bind_groups.get(dir), &[]);
-        cpass.dispatch(xgroups, ygroups, 1);
+        cpass.set_bind_group(0, self.bind_groups.get(dir), &[]);
+        cpass.dispatch_workgroups(xgroups, ygroups, 1);
+        //cpass.dispatch(xgroups, ygroups, 1);
 
         self.frame_num += 1;
     }
